7dffcf056d505bed2d7eb14c9fe8ba8f6378a7f2
Andrew <crosio@dubizzle.com>
2012-08-02 00:29:57 +0400

    More tests.

--- queryset_iterator.py
+++ queryset_iterator.py
@@ -5,9 +5,10 @@ This function is useful for iterating over large querysets with Django.
 import gc
 
 
-def queryset_iterator(queryset, batchsize = 500, gc_collect = True):
+def queryset_iterator(queryset, batchsize=500, gc_collect=True):
     """
-    Obtain a generator that can be used to iterator over the queryset in batches.
+    Obtain a generator that can be used to iterator
+    over the queryset in batches.
     Keyword Arguments:
     queryset -- The queryset to iterate over in batches.
     batchsize -- The batch size used to process the queryset. Defaults to 500.
@@ -16,24 +17,25 @@ def queryset_iterator(queryset, batchsize = 500, gc_collect = True):
     # Acquire a distinct iterator of the primary keys within the queryset.
     # This will be maintained in memory (or a temporary table) within the
     # database and iterated over, i.e. we will not copy and store results.
-    iterator = queryset.values_list('pk', flat = True).distinct().iterator()
+    iterator = queryset.values_list('pk', flat=True).distinct().iterator()
     # Begin main logic loop. Will loop until iterator is exhausted.
     while True:
-        pk_buffer = []
-        buffer_len = 0
         try:
+            pk_buffer = []
+            buffer_len = 0
             # Consume queryset iterator until batch is reached or the
             # iterator has been exhausted.
             while buffer_len < batchsize:
                 pk_buffer.append(iterator.next())
                 buffer_len += 1
         except StopIteration:
-            # Break out of the loop once the queryset iterator has been consumed.
+            # Break out of the loop once the queryset
+            # iterator has been consumed.
             break
         finally:
             # Use the original queryset to obtain the proper results.
             # Once again using an iterator to keep memory footprint low.
-            for result in queryset.filter(pk__in = pk_buffer).iterator():
+            for result in queryset.filter(pk__in=pk_buffer).iterator():
                 yield result
 
             if gc_collect:
--- test.py
+++ test.py
@@ -2,10 +2,9 @@ from queryset_iterator import queryset_iterator
 from mock import Mock
 import unittest
 
-
 VERBOSITY = 2
 
-
+#todo: refactor and clean-up everything
 def get_filter_side_effect(result_set):
     def _get_filter_side_effect_inner(pk__in):
         filter_mock = Mock()
@@ -24,21 +23,46 @@ def get_iterator_side_effect(result_set, pk__in):
     return _get_iterator_side_effect_inner
 
 
+def get_queryset_mock(result_set):
+    queryset_mock = Mock()
+    iterator_results = (result['pk'] for result in result_set)
+    values_list = queryset_mock.values_list.return_value
+    distinct = values_list.distinct.return_value
+    distinct.iterator.return_value = iterator_results
+    queryset_mock.filter.side_effect = get_filter_side_effect(result_set)
+    return queryset_mock
+
+
+#todo: nice programmatic way to test data sets.
+#todo: some fixtures
 class QuerySetIteratorTest(unittest.TestCase):
-    def test_return_values_corrent(self):
-        result_set = [{ 'pk' : i, 'info' : 'item number %d' % i }
-                        for i in xrange(1, 1000)]
-        queryset_mock = Mock()
-        iterator_results = (result['pk'] for result in result_set)
-        values_list = queryset_mock.values_list.return_value
-        distinct = values_list.distinct.return_value
-        distinct.iterator.return_value = iterator_results
-#        values_list_iterator = distinct.iterator.return_value
-#        values_list_iterator.return_value = iterator_results
-        queryset_mock.filter.side_effect = get_filter_side_effect(result_set)
+    def setUp(self):
+        self.result_set = [{ 'pk' : i, 'info' : 'item number %d' % i }
+                           for i in xrange(1, 1000)]
+
+    def test_return_values_correct(self):
+        #todo: do this with different sets
+        queryset_mock = get_queryset_mock(self.result_set)
         test_results_iterator = queryset_iterator(queryset_mock, batchsize = 5)
         full_results = [result for result in test_results_iterator]
-        self.assertEqual(full_results, result_set)
+        self.assertEqual(full_results, self.result_set)
+
+    def test_retrieves_in_batches(self):
+        #todo: do this with different batch sizes
+        queryset_mock = get_queryset_mock(self.result_set)
+        test_results_iterator = queryset_iterator(queryset_mock, batchsize = 1)
+        #todo: refactor and abstract this
+        first_pk = test_results_iterator.next()['pk']
+        mock_calls = queryset_mock.filter.mock_calls
+        self.assertEqual(len(mock_calls), 1)
+        self.assertEqual(str(mock_calls[-1]), 'call(pk__in=[%d])' % first_pk)
+        second_pk = test_results_iterator.next()['pk']
+        mock_calls = queryset_mock.filter.mock_calls
+        self.assertEqual(len(mock_calls), 2)
+        self.assertEqual(str(mock_calls[-1]), 'call(pk__in=[%d])' % second_pk)
+
+    def test_gc_collect_at_end_of_batch(self):
+        self.skipTest('Not Implemented Yet')
 
 
 def main():
